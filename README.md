1cad8c1c214a094d308913fab0374579ddb08ba3
Seraphin Berger <mail@mcmodersd.de>
2025-09-09 13:41:19 +0200

    init rewrite 1.2

--- README.md
+++ README.md
@@ -22,27 +22,28 @@ Add the dependency to your `pom.xml` file:
 <dependency>
     <groupId>de.MCmoderSD</groupId>
     <artifactId>Encryption</artifactId>
-    <version>1.1.0</version>
+    <version>1.2.0</version>
 </dependency>
 ```
 
 
 ## Usage Example
+
 ```java
-import de.MCmoderSD.encryption.Encryption;
+import de.MCmoderSD.encryption.core.Encryption;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         // Create a new instance of the Encryption class
         Encryption encryption = new Encryption("MyStrongPassword");
-        
+
         // Encrypt a string
         String encryptedString = encryption.encrypt("Hello, World!");
-        
+
         // Decrypt the encrypted string
         String decryptedString = encryption.decrypt(encryptedString);
-        
+
         // Print the encrypted and decrypted strings
         System.out.println("Encrypted String: " + encryptedString);
         System.out.println("Decrypted String: " + decryptedString);
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,19 +1,29 @@
-import de.MCmoderSD.encryption.Encryption;
+import de.MCmoderSD.encryption.core.Encryption;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Create a new instance of the Encryption class
-        Encryption encryption = new Encryption("MyStrongPassword");
+        // Variables
+        String originalString = "Hello, World!";
+        String password = "securepassword";
 
-        // Encrypt a string
-        String encryptedString = encryption.encrypt("Hello, World!");
+        // Initialize Encryption
+        Encryption encryption = new Encryption(password);
 
-        // Decrypt the encrypted string
-        String decryptedString = encryption.decrypt(encryptedString);
+        // Encrypt first time
+        String encryptedString1 = encryption.encrypt(originalString);
+        System.out.println("Encrypted String 1: " + encryptedString1);
 
-        // Print the encrypted and decrypted strings
-        System.out.println("Encrypted String: " + encryptedString);
-        System.out.println("Decrypted String: " + decryptedString);
+        // Encrypt second time
+        String encryptedString2 = encryption.encrypt(originalString);
+        System.out.println("Encrypted String 2: " + encryptedString2);
+
+        // Decrypt both
+        String decryptedString1 = encryption.decrypt(encryptedString1);
+        String decryptedString2 = encryption.decrypt(encryptedString2);
+
+        // Output results
+        System.out.println("Decrypted String 1: " + decryptedString1);
+        System.out.println("Decrypted String 2: " + decryptedString2);
     }
 }
